5d9c1667813b0862ec73dadc4e33cd0d64714f50
Leslie <leslie.appl@gmail.com>
2020-12-19 16:23:19 +0800

    Initial Commit

---
+++ ObjectModel1219/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjectModel1219
+//
+//  Created by leslie on 12/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
